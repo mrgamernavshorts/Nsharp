97c239c16d537acdd23ba65bed53790adc64a537
mrgamernavshorts <137981200+mrgamernavshorts@users.noreply.github.com>
2024-08-27 00:22:42 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,60 @@
 print('N# Complier')
 q = input('enter file name :')
 var = ''
+num1 = None
+num2 = None
 with open(q,'r') as f:
     code = f.readlines()
-if code[0].strip().startswith('write:'):
-    print(code[0].strip()[6:])
-elif code[0].strip().startswith('createvar:'):
-    var = code[0].strip()[10:]
-    print(f'the value is stored as {var}')
-    w = input('print it? :')
-    if w == 'y':
+for line in code:
+    line = line.strip()
+    if line.startswith('write:'):
+        print(line[6:])
+    elif line.startswith('createvar:input:'):
+        var = input('enter the input:')
+        print(f'variable stored as {var}')
+    elif line.startswith('createvar:'):
+        var = line[10:]
+        print(f'variable stored as {var}')
+    elif line.startswith('printvar:'):
         print(var)
-    else:
-        pass
-if code[1].strip().startswith('write:'):
-    print(code[1].strip()[6:])
-if code[1].strip().startswith('createvar:'):
-    var = code[1].strip()[10:]
-    print(f'the value is stored as {var}')
-    w = input('print it? :')
-    if w == 'y':
-        print(var)
-    else:
-        pass
+    elif line.startswith('add:num1'):
+        num1 = int(line[9:])
+    elif line.startswith('add:num2:'):
+        num2 = int(line[9:])
+    elif line.startswith('addrun:'):
+        if num1 is not None and num2 is not None:
+            addans = num1 + num2
+            print(addans)
+        else:
+            print('num1 and num2 not defined')
+    elif line.startswith('sub:num1'):
+        num1 = int(line[9:])
+    elif line.startswith('sub:num2:'):
+        num2 = int(line[9:])
+    elif line.startswith('subrun:'):
+        if num1 is not None and num2 is not None:
+            subans = num1 - num2
+            print(subans)
+        else:
+            print('num1 and num2 not defined')
+    elif line.startswith('div:num1'):
+        num1 = int(line[9:])
+    elif line.startswith('div:num2:'):
+        num2 = int(line[9:])
+    elif line.startswith('divrun:'):
+        if num1 is not None and num2 is not None:
+            divans = num1 / num2
+            print(divans)
+        else:
+            print('num1 and num2 not defined')
+    elif line.startswith('multi:num1'):
+        num1 = int(line[11:])
+    elif line.startswith('multi:num2:'):
+        num2 = int(line[11:])
+    elif line.startswith('multirun:'):
+        if num1 is not None and num2 is not None:
+            multians = num1 * num2
+            print(multians)
+        else:
+            print('num1 and num2 not defined')
 input('exit ? :')
